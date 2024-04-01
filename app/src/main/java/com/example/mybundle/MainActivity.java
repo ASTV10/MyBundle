1526705a22d1ebfad90258bcbf807765944e4daa
ASTV10 <122161737+ASTV10@users.noreply.github.com>
2024-04-01 16:08:12 +0600

    Initial commit

---
+++ app/src/main/java/com/example/mybundle/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.mybundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.container,
+                            VioletFragment.class,null).commit();
+        }
+    }
+}
